aa49ddde340eeca89138d0f56e24fb798919468e
kahenglish <kahenglish1@gmail.com>
2024-10-23 10:03:10 +0000

    앱만들기 시작

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title("👋🏻 Streamlit 앱 만들기")
-st.subheader("이 페이지는 실습 페이지입니다.")
-st.write("오른쪽 위의 'fork' 버튼을 눌러주세요. 이 페이지와 앱이 그대로 복사됩니다.")
+st.title("👋🏻 연수 실습 페이지(1023)")
+st.subheader("안녕하세요")
+st.write("안녕안녕하세요.")
+st.write("https://padlet.com/sbhath17/sw-ai-streamlit-fuyp1hmna3pr9n9i")
+st.link_button("아현 github 페이지 바로가기!", "https://github.com/kahenglish")
 
-st.link_button("streamlit 매뉴얼 페이지 바로가기!", "https://surish.notion.site/streamlit-113eef51495c8083986cc65f2d07470c?pvs=73")
-
-# st.success("초록색 창")
-# st.error("빨간색 창")
+st.success("초록색 창")
+st.error("빨간색 창")
 st.info("파란색 창")
-# st.warning("노란색 창") # ctrl+/ : 주석처리
-st.image("https://media.giphy.com/media/8bE0EERrvXkq5S9BCa/giphy.gif?cid=ecf05e479tvvde0o3d3do3g67yoq0jn4zzyg9hyt63stuf0x&ep=v1_gifs_related&rid=giphy.gif&ct=g", caption="Welcome to coding world") 
+st.warning("노란색 창") # ctrl+/ : 주석처리
+st.image("https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXR4NXdhMG01NGYwaXJnc21vcjJncGthN2hnazAzN2NpbmgxcGZkeiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Pt4ARBAFoiTRuIEWkZ/giphy.gif", caption="happy happy") 
+st.video("https://www.youtube.com/watch?v=utIbM9Iua-kn")
